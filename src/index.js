044a8c8702156399604811c1350c298ba73aaeec
thegoodcode <rhapsody@tuta.io>
2021-10-05 17:59:59 +0100

    add html webpack plugin to take care of html bundling

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const app = document.getElementById('root')
+const heading = document.createElement('h1')
+heading.textContent = 'Hello World'
+app.appendChild(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: {
@@ -7,5 +8,12 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, './dist'),
 		filename: '[name].bundle.js'
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'webpack boilerplate',
+			template: path.resolve(__dirname, './src/template.html'),
+			filename: 'index.html'
+		})
+	]
 }
