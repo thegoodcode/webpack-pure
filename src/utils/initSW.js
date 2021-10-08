ac7f7fac56c5a082e0e057af24764cb4d73f1de8
thegoodcode <rhapsody@tuta.io>
2021-10-08 17:05:24 +0100

    add basic service worker

---
+++ src/utils/initSW.js
@@ -0,0 +1,11 @@
+export const initServiceWorker = () => {
+	if (!navigator.serviceWorker) return
+	navigator.serviceWorker
+		.register('../sw.js')
+		.then(() => {
+			console.log('sw registered successfully!')
+		})
+		.catch(error => {
+			console.log('Some error while registering sw:', error)
+		})
+}
