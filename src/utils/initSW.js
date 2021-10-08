e6d64faf89494be7613e359dd60b9aecb8546a83
thegoodcode <rhapsody@tuta.io>
2021-10-08 17:11:57 +0100

    update sw path

--- src/utils/initSW.js
+++ src/utils/initSW.js
@@ -1,7 +1,7 @@
 export const initServiceWorker = () => {
 	if (!navigator.serviceWorker) return
 	navigator.serviceWorker
-		.register('../sw.js')
+		.register('./sw.js')
 		.then(() => {
 			console.log('sw registered successfully!')
 		})
