642e31b0ecbd86bf9164fbf16eb0c7c5ca099b52
thegoodcode <rhapsody@tuta.io>
2021-10-08 16:10:34 +0100

    add dev & prod webpack config and remove single config file

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+	mode: 'development',
+	devServer: {
+		historyApiFallback: true,
+		static: {
+			directory: path.resolve(__dirname, './dist')
+		},
+		open: true,
+		compress: true,
+		port: 5000,
+		hot: true
+	},
+	module: {
+		rules: [
+			// CSS, PostCSS, and Sass
+			{
+				test: /\.(scss|css)$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+			}
+		]
+	}
+})
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const common = require('./webpack.common.js')
+const path = require('path')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+	output: {
+		publicPath: './',
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].[contenthash].bundle.js'
+	},
+	mode: 'production',
+	module: {
+		rules: [
+			// CSS, PostCSS, and Sass
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+							sourceMap: false,
+							modules: true
+						}
+					},
+					'postcss-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		// Extracts CSS into separate files
+		new MiniCssExtractPlugin({
+			filename: '/styles/[name].[contenthash].css',
+			chunkFilename: '[id].css'
+		})
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin(), '...'],
+		runtimeChunk: {
+			name: 'runtime'
+		}
+	},
+	performance: {
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000
+	}
+})
